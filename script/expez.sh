ac52d5a69fff17a0e9bcd56d320225360685d02f
jeldridgeTS <jeldridge@turbosquid.com>
2016-05-31 15:57:52 -0500

    Finish basic directory and file creation for basic flow

--- script/expez.sh
+++ script/expez.sh
@@ -1,14 +1,40 @@
 #!/bin/sh
 
-function help() {
+ACTION=$1
+APP_TYPE=$2
+APP_NAME=$3
+
+help() {
   echo ""
   echo "    expez - A simple script that sets up project structures for express.js apps.";
   echo ""
   echo "    expez create [basic] or [mvc] - creates an express app folder structure";
 }
 
-function create() {
-  echo "I am create"
+make_dir() {
+  if [[ $1 = basic ]]; then
+    echo "making $1 folder structure for: $APP_NAME"
+    mkdir -p $APP_NAME
+    mkdir -p $APP_NAME/public/javascript/lib
+    mkdir -p $APP_NAME/public/javascript/scripts
+    mkdir -p $APP_NAME/views
+    mkdir -p $APP_NAME/views/partials
+
+    touch $APP_NAME/app.js
+    touch $APP_NAME/views/partials/header.ejs
+    touch $APP_NAME/views/partials/footer.ejs
+  elif [[ $1 = mvc ]]; then
+    echo "making $1 folder structure for: $APP_NAME"
+  fi
+}
+
+npm_init() {
+  cd $APP_NAME
+  npm init
+}
+
+npm_install_dependencies() {
+  npm install --save express ejs request
 }
 
 if [[ $1 ]]; then
@@ -21,12 +47,13 @@ if [[ $1 ]]; then
         echo "You need an app root name"
       else
         if [[ $2 = "basic" ]]; then
-          mkdir -p $3 # TODO mkdir func for mvc structure
+          make_dir basic;
         elif [[ $2 = "mvc" ]]; then
-          mkdir -p $3 # TODO mkdir func for mvc structure
+          make_dir mvc;
         fi
       fi
-      create;
+      npm_init;
+      npm_install_dependencies;
     ;;
   esac
 fi
